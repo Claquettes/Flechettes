16b7682a95da57f3289115b37b921a4b925d351b
maxime ANTOINE <maxime.antoine001@gmail.com>
2023-01-06 00:33:42 +0100

    Ajout bouton

--- pfff/App.js
+++ pfff/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>FLéchettes</Text>
+      <Button
+        title='Start a game'
+        color='#287DD6'
+      />
       <StatusBar style="auto" />
     </View>
   );
