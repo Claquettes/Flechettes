e0cb8753d8feb0fce01c166df3c01b53f82218c5
maxime ANTOINE <maxime.antoine001@gmail.com>
2023-01-09 11:07:52 +0100

    Debut de bdd local

--- pfff/ManagePlayers.js
+++ pfff/ManagePlayers.js
@@ -1,7 +1,35 @@
 import {View, Text, TouchableOpacity} from 'react-native';
+import {AsyncStorage} from '@react-native-async-storage/async-storage';
 import {styles} from "./styles/MainStylesheet";
-
 const ManagePlayers = ({navigation}) => {
+
+    const storeData = async (value) => {
+        try {
+          const jsonValue = JSON.stringify(value)
+          await AsyncStorage.setItem('players', jsonValue)
+        } catch (e) {
+          // saving error
+        }
+    }
+
+    const getData = async () => {
+        try {
+          const jsonValue = await AsyncStorage.getItem('players')
+          console.log(await jsonValue); 
+          return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch(e) {
+          // error reading value
+        }
+      }
+    
+      let obj = {  
+        name: 'Michal',  
+        email: 'michal@gmail.com',  
+        city: 'New York',  
+      }
+
+      storeData(obj);
+      console.log(getData())
     
     return (
         <View style={styles.containerMain}>
