bdc1c8bd5916bdab9ee7c135ed518191bc6a2e0d
Claquettes <math@claq.fr>
2023-01-11 21:10:44 +0100

    Added debug screen, fixed bug in party mode, fixed the function that looks if you won (for now you need to press any button for the function to work)

---
+++ temporaire.groovy
@@ -0,0 +1,9 @@
+if((Math.random() < 0.95)&&(Math.random() > 0.475)){ //47.5% de chance
+                            triggerNormal();
+                        }
+                        if(Math.random() >= 0.95){ //5% de chance
+                            triggerGoldenCarrot();
+                        }
+                        else{
+                            triggerNormal();
+                        }
